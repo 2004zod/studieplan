c6dfa5aa4e3dcd93445aaa5b8ebf7a5c4017e76a
Venya18 <59624457+Venya18@users.noreply.github.com>
2025-10-15 11:55:17 +0200

    Update studieplan.py

--- studieplan.py
+++ studieplan.py
@@ -1,12 +1,12 @@
-studieplan = {}
-
+studieplan = {}     # Этот файл отвечает за хранение и управление структурой учебного плана
+                    # Создает глобальный словарь
 def legg_til_i_studieplan():
     semester = input("Skriv semester (for eksempel 'V2025'): ")
     emnekode = input("Skriv emnekode: ")
 
-    if semester not in studieplan:
+    if semester not in studieplan:    # Если семестр не в словаре, то он создается
         studieplan[semester] = []
-    studieplan[semester].append(emnekode)
+    studieplan[semester].append(emnekode)    # Код предмета добавляется в список этого семестра
     print(f"Emne {emnekode} lagt til i {semester}.")
 
 def skriv_ut_studieplan():
@@ -14,5 +14,5 @@ def skriv_ut_studieplan():
         print("Ingen studieplan registrert.")
     else:
         print("\nStudieplan:")
-        for semester, emner in studieplan.items():
+        for semester, emner in studieplan.items():    # Проход по всем семестрам в словаре и вывод кодов предметов с запятой
             print(f"{semester}: {', '.join(emner)}")
